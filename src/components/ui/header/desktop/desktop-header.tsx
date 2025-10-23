eb6b4f606845eff4fbcd192748303b48952ae98b
ParsaProg <parsashaabani3@gmail.com>
2025-10-23 16:24:45 +0330

    desktop header edits

--- src/components/ui/header/desktop/desktop-header.tsx
+++ src/components/ui/header/desktop/desktop-header.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function DesktopHeader({ navItems }: { navItems: Array<any> }) {
   return (
-    <div className="[@media(max-width:1495px)]:hidden visible w-[90%] mx-auto text-white py-3 bg-[#4c5ed5] backdrop-blur-lg h-auto flex border-b-[1px] border-t-white gap-x-1">
+    <div className="[@media(max-width:1495px)]:hidden visible w-[90%] mx-auto text-white py-3 h-auto flex border-b-[1px] border-t-white gap-x-1">
       {navItems.map((nav, _i) => {
         return (
           <div key={_i} className={`relative ${nav.subs ? "group" : ""}`}>
             <Link
               href={nav.route}
-              className={`font-bold p-3 hover:bg-white hover:text-[#4C5FD5] rounded-lg transition-colors duration-200 text-lg flex items-center gap-x-2`}
+              className={`font-thin p-3 hover:bg-white hover:text-[#4C5FD5] rounded-lg transition-colors duration-200 text-lg flex items-center gap-x-2`}
               aria-haspopup={nav.subs ? "menu" : undefined}
             >
               {nav.icon}
@@ -20,7 +20,7 @@ export default function DesktopHeader({ navItems }: { navItems: Array<any> }) {
             {nav.subs && (
               <div
                 className="absolute left-0 top-[50px] w-full bg-[#4C5FD5]
-                 border-[0.5px] border-white rounded-lg p-3 z-10
+                 border-[0.5px] border-[#e2e6f0d5] rounded-lg p-3 z-10
                  opacity-0 translate-y-1 scale-95 pointer-events-none
                  group-hover:opacity-100 group-hover:translate-y-0 group-hover:scale-100 group-hover:pointer-events-auto
                  group-focus-within:opacity-100 group-focus-within:translate-y-0 group-focus-within:scale-100
