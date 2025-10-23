21d0bad5419f1c1b1c9b4256a9e30770835dac41
ParsaProg <parsashaabani3@gmail.com>
2025-10-23 16:23:31 +0330

    client layout added to project

--- src/app/clientLayout.tsx
+++ src/app/clientLayout.tsx
@@ -1,3 +1,12 @@
-export default function ClientLayout({children}: {children: React.ReactNode}){
-    return <div dir="rtl">{children}</div>
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div dir="rtl">
+      <div className="mt-[101px]"></div>
+      {children}
+    </div>
+  );
 }
