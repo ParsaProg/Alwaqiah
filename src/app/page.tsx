5710999948defe8a73d912a4c1aa2e4d364c04f1
ParsaProg <parsashaabani3@gmail.com>
2025-10-30 15:43:11 +0330

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { HeroSection } from "@/components/ui/hero/HeroSection";
+import BlueContainerAye from "@/components/ui/landing/BlueContainerAye";
+import ErabQuranSection from "@/components/ui/landing/ErabQuran";
 import QuranSearch from "@/components/ui/landing/QuranSearch";
 import QuranContainerTypes from "@/components/ui/landing/QuranTypesContainers";
 
@@ -7,6 +9,8 @@ export default function Home(){
     <HeroSection />
     <QuranContainerTypes />
     <QuranSearch />
+    <BlueContainerAye />
+    <ErabQuranSection />
     <div className="mt-[100px]"></div>
   </div>
 }
