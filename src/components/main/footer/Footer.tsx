1e9777c8e3e835ec5047109f9c70e00600e4ed69
ParsaProg <parsashaabani3@gmail.com>
2025-10-30 15:43:15 +0330

    footer.tsx

---
+++ src/components/main/footer/Footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <div className="text-center flex p-5 flex-col items-center gap-y-2 w-full py-5 text-white bg-primary-Light">
+      <section className="text-center flex items-center gap-x-3 text-sm flex-wrap">
+        <h3 className="text-center">درباره‌ما</h3>
+        |
+        <h3 className="text-center">تماس با ما</h3>
+        |
+        <h3 className="text-center">نظرات مسلمانان</h3>
+      </section>
+      <p className="text-sm">
+        حقوق مادی و معنوی اين پايگاه متعلق به مرکز تحقیقات کامپیوتری علوم اسلامی
+        است و نشر غیرمجاز محتوای آن پیگرد قانونی دارد
+      </p>
+      <h1>ساخته شده با ❤️ برای ایران</h1>
+    </div>
+  );
+}
