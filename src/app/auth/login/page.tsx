7920a5866526a2cb6ea54c80bd4fb72cf3c8c67d
ParsaProg <parsashaabani3@gmail.com>
2025-11-01 01:49:26 +0330

    add options section to bottom of the form in auth page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -133,48 +133,74 @@ export default function LoginPage() {
         className=" [@media(min-width:1060px)]:hidden visible animate-bounce text-primary-Light"
         size={20}
       />
-      <div className="w-full [@media(min-width:601px)]:max-w-[600px] max-w-[90%] rounded-xl shadow-[0px_0px_15px_5px] flex text-center flex-col gap-y-3 shadow-[#CBD6FD] p-8 text-black">
-        <h1 className="mt-3 font-bold text-confirmHover-Light [@media(max-width:1060px)]:text-xl text-3xl">
-          ورود به سامانه‌ی الواقعه
-        </h1>
+      <section className="flex flex-col w-full rounded-xl gap-y-5 [@media(min-width:601px)]:max-w-[600px] max-w-[90%] ">
+        <div className="w-full rounded-xl shadow-[0px_0px_15px_5px] flex text-center flex-col gap-y-3 shadow-[#CBD6FD] p-8 text-black">
+          <h1 className="mt-3 font-bold text-confirmHover-Light [@media(max-width:1060px)]:text-xl text-3xl">
+            ورود به سامانه‌ی الواقعه
+          </h1>
 
-        <section className="flex flex-col items-start mt-5">
-          <h2 className="font-bold text-black text-xl">نام کاربری</h2>
-          <div className="relative text-slate-500 w-full">
-            <User className="absolute top-[50%]  right-3" size={20} />
-            <input
-              type="text"
-              placeholder="نام کاربری خود را وارد کنید"
-              className="w-full py-5 px-10 outline-none rounded-xl focus:ring-2 focus:ring-confirmHover-Light focus:ring-offset-2 border-[1px] mt-5 border-slate-300 placeholder:text-slate-500 transition-shadow duration-200"
-            />
-          </div>
-        </section>
+          <section className="flex flex-col items-start mt-5">
+            <h2 className="font-bold text-black text-xl">نام کاربری</h2>
+            <div className="relative text-slate-500 w-full">
+              <User className="absolute top-[50%]  right-3" size={20} />
+              <input
+                type="text"
+                placeholder="نام کاربری خود را وارد کنید"
+                className="w-full py-5 px-10 outline-none rounded-xl focus:ring-2 focus:ring-confirmHover-Light focus:ring-offset-2 border-[1px] mt-5 border-slate-300 placeholder:text-slate-500 transition-shadow duration-200"
+              />
+            </div>
+          </section>
 
-        <section className="flex flex-col items-start mt-5">
-          <h2 className="font-bold text-black text-xl">رمز عبور</h2>
-          <div className="relative text-slate-500 w-full">
-            <Lock className="absolute top-[50%]  right-3" size={20} />
-            <Eye
-              className="absolute top-[50%] cursor-pointer left-5"
-              size={20}
-            />
-            <input
-              type="text"
-              placeholder="رمز عبور خود را وارد کنید"
-              className="w-full py-5 px-10 outline-none rounded-xl focus:ring-2 focus:ring-confirmHover-Light focus:ring-offset-2 border-[1px] mt-5 border-slate-300 placeholder:text-slate-500 transition-shadow duration-200"
-            />
-          </div>
-        </section>
+          <section className="flex flex-col items-start mt-5">
+            <h2 className="font-bold text-black text-xl">رمز عبور</h2>
+            <div className="relative text-slate-500 w-full">
+              <Lock className="absolute top-[50%]  right-3" size={20} />
+              <Eye
+                className="absolute top-[50%] cursor-pointer left-5"
+                size={20}
+              />
+              <input
+                type="text"
+                placeholder="رمز عبور خود را وارد کنید"
+                className="w-full py-5 px-10 outline-none rounded-xl focus:ring-2 focus:ring-confirmHover-Light focus:ring-offset-2 border-[1px] mt-5 border-slate-300 placeholder:text-slate-500 transition-shadow duration-200"
+              />
+            </div>
+          </section>
 
-        <button className="hover:bg-blue-800 transition-colors duration-200 rounded-xl p-5 text-white bg-primary-Light mt-5 text-lg">
-          ورود به سامانه
-        </button>
+          <button className="hover:bg-blue-800 transition-colors duration-200 rounded-xl p-5 text-white bg-primary-Light mt-5 text-lg">
+            ورود به سامانه
+          </button>
 
-        <section className="mt-5 text-primary-Light w-full flex justify-between items-center">
-          <div className="cursor-pointer">ثبت نام کنید</div>
-          <div className="cursor-pointer">فراموشی رمز‌عبور؟</div>
-        </section>
-      </div>
+          <section className="mt-5 text-primary-Light w-full flex justify-between items-center">
+            <div className="cursor-pointer">ثبت نام کنید</div>
+            <div className="cursor-pointer">فراموشی رمز‌عبور؟</div>
+          </section>
+        </div>
+        <div className="w-full rounded-xl shadow-[0px_0px_15px_5px]  text-center gap-y-3 shadow-[#CBD6FD] p-8 text-black flex flex-col items-start">
+          <h1 className="font-bold text-2xl text-confirmHover-Light">
+            امکانات سامانه
+          </h1>
+          <ul className="text-start text-lg flex flex-col gap-y-3">
+            <li className="flex items-center gap-x-2">
+              <div className="rounded-full bg-black w-2 h-2"></div>
+              مطالعه قرآن با ترجمه و تفسیر
+            </li>
+            <li className="flex items-center gap-x-2">
+              <div className="rounded-full bg-black w-2 h-2"></div>جستجوی
+              پیشرفته در آیات قرآن{" "}
+            </li>
+            <li className="flex items-center gap-x-2">
+              <div className="rounded-full bg-black w-2 h-2"></div>ذخیره آیات
+              مورد علاقه
+            </li>
+            <li className="flex items-center gap-x-2">
+              <div className="rounded-full bg-black w-2 h-2"></div>پخش صوت
+              قاریان مختلف
+            </li>
+          </ul>
+        </div>
+      </section>
+
       <div className="[@media(max-width:1060px)]:flex hidden w-[90%] rounded-xl shadow-[0px_0px_15px_5px]  text-center flex-col gap-y-3 shadow-[#CBD6FD] p-10 text-black">
         <h1 className="font-bold untihamo text-2xl">
           بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
