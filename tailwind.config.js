ae04e85750b9c957741853b1e347d5acf8277fcd
ParsaProg <parsashaabani3@gmail.com>
2025-11-08 01:22:22 +0330

    tailwind.config.js file changes for the update from 3 => 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,10 @@
 module.exports = {
   darkMode: "class",
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          Light: "#4C5FD5",
-          Dark: "",
-        },
-        secondry: {
-          Light: "#FFB400",
-          Dark: "",
-        },
-        confirmHover: {
-          Light: "#7C5CE3",
-          Dark: ""
-        }
-      },
-    },
-  },
   plugins: [],
 };
